b0019e21d18332af236cdbe0684e09bf3b168b1e
RBizonDota <rbaybarin@icloud.com>
2018-09-13 00:26:34 +0300

    3rd commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-		int a = 0;
+		int b = 0;
+		b++;
     	System.out.println("Hello World\n");
     	Server a = new Server();  
      	System.exit(0);
