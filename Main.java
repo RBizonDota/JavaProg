69879af0bd0c37783c7cd5e844651a420abe407a
avasilyev1998 <avasilyev1998@mail.ru>
2018-09-13 00:15:25 +0300

    Snd commit

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+		int a = 0;
     	System.out.println("Hello World\n");
     	Server a = new Server();  
      	System.exit(0);
-	// юху коммит git
+	// юху коммит git мой fork
 	}
 }
