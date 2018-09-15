89635673b54a572003105f549ed10ea253e299c4
RBizonDota <rbaybarin@icloud.com>
2018-09-15 17:06:51 +0300

    Client commands added (fixed)

--- ServClient/src/Client/Client.java
+++ ServClient/src/Client/Client.java
@@ -7,43 +7,44 @@ import java.util.*;
 
 class Client{
     // Поля
-    private int PORT_NUMBER = 3345;
-    private String HOSTNAME = "127.0.0.1";
-    private String CLIENTNAME = "Roman";
+    private int PORT_NUMBER = 3345; //Порт хоста
+    private String HOSTNAME = "127.0.0.1"; //IP хоста
+    private String CLIENTNAME = "Roman"; //Имя клиента, видимое на сервере
+    private boolean flag = true; //Флаг работы системы
     // Конструкторы
     Client(){}
     // Методы
     public int start()
     {
-        String exitConditionStr="";
-       cycle: while (!(exitConditionStr.equals("-dc"))) {
+       while (flag) {
             try {
                 System.out.print("Enter command: ");
                 Scanner scan = new Scanner(System.in);
                 String clientString = scan.nextLine();
-                exitConditionStr = clientString;
                 if( clientString.charAt(0) == '-')
                 {
                     runClientCommand(clientString);
-                    continue cycle;
                 }
                 //Попытка подключения
-                Socket sock = new Socket(HOSTNAME, PORT_NUMBER);
-                PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
-                out.println(CLIENTNAME);
-                //Вывод
-                out.println(clientString);
-                out.flush();
-                BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                System.out.println(br.readLine());//Дублирование сообщения клиента
-                long j = Long.parseLong(br.readLine());//Сообщение о количестве строк
-                for (long i = 0; i < j; i++)
-                    System.out.println(br.readLine());//Построчный вывод ответа
-                out.println(0);
-                out.close();
-                br.close();
-                sock.close();
-            } catch (Exception e) {
+                else{
+                    Socket sock = new Socket(HOSTNAME, PORT_NUMBER);
+                    PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+                    out.println(CLIENTNAME);//Отправка имени клиента
+                    //Вывод
+                    out.println(clientString);//
+                    out.flush();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                    System.out.println(br.readLine());//Дублирование сообщения клиента
+                    long j = Long.parseLong(br.readLine());//Сообщение о количестве строк
+                    for (long i = 0; i < j; i++)
+                        System.out.println(br.readLine());//Построчный вывод ответа
+                    out.println(0);//Костыль, гарант, что сокет не закроется раньше времени
+                    out.close();
+                    br.close();
+                    sock.close();
+                }
+            }
+            catch (Exception e) {
                 e.printStackTrace();
                 return 1;
             }
@@ -59,7 +60,9 @@ class Client{
     private void runClientCommand(String clientCommand)
     {
         //Вернется в начало цикла по continue cycle и завершится
-        if(clientCommand.equals("-dc")){}
+        if(clientCommand.equals("-dc")){
+            flag = false;
+        }
         //Смена HOSTNAME по команде Change HOSTNAME
         else if(clientCommand.equals("-chh"))
         {
