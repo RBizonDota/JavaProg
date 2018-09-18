63feacdd8d1a01fab838fbfbed61679911212acf
RBizonDota <rbaybarin@icloud.com>
2018-09-18 09:35:14 +0300

    CVAR1 ended)

--- src/Server/FuncStack.java
+++ src/Server/FuncStack.java
@@ -17,7 +17,11 @@ public class FuncStack {
         String res = h + ":" + m + ":" + s;
         return res;
     }
-
+    public static int fact(int n)
+    {
+        if (n == 0) return 1;
+        return n * fact(n-1);
+    }
     public int rooter(String s, PrintWriter out, String clientName, int code){
         try{
             switch(s){
@@ -49,7 +53,7 @@ public class FuncStack {
                     func_dc(out,clientName,s,code);
                     return 1;
                 }
-                case "code":{
+                case "cvar1":{
                     func_code(out,clientName,s,code);
                 }
                 break;
@@ -112,7 +116,7 @@ public class FuncStack {
     }
     int func_code(PrintWriter out, String clientName,String cmd,int code){
 
-        int ax_start = 0b1101;
+        int ax_start = 0b1010;
         int ax = ax_start;
         int gx = 0b1011;
         int sx;
@@ -126,7 +130,7 @@ public class FuncStack {
             }
         }
         sx = ax ^ (lx);
-        out.println(3);
+        out.println(12);
         out.println(TimeStamp() + " - " + "Server: " + "AX = "+Integer.toBinaryString(ax_start));
         out.println(TimeStamp() + " - " + "Server: " + "SX = "+Integer.toBinaryString(sx));
         lx = sx;
@@ -137,6 +141,41 @@ public class FuncStack {
             }
         }
         out.println(TimeStamp() + " - " + "Server: " + "SX%GX = "+Integer.toBinaryString(lx));
+
+        int l;
+        int buf;
+        int st=0;
+        int[] stat = new int[8];
+        int[] stall = new int[8];
+        for (int exept = 0;exept<128;exept++)
+        {
+            st = 0;
+            buf = exept;
+            for(int i = 6;i>-1;i--)
+            {
+                l = (int)(Math.pow(2,(double)(i)));
+                if(buf/l==1){
+                    st++;// Получили количество значащих единиц в векторе ошибок
+                    buf -=l;
+                }
+            }
+            stall[st]++;
+            lx = sx;
+            lx^=exept;//выражение с ошибкой
+            for (int i = 3;i>-1;i--){
+                int lol = (int)(Math.pow(2,(double)(i+3)));
+                if(lx/lol==1){
+                    lx^=gx<<i;
+                }
+            }
+            if(lx!=0)
+                stat[st]++;
+        }
+        out.println(TimeStamp() + " - " + "Server: " + "Decoding:");
+        for (int i=1;i<8;i++) {
+            out.println(TimeStamp() + " - " + "Server: " + "Number of changed bits = "+i+", number of codes = "+stall[i]+", detected exceptions = "+stat[i]+", "+(double)(stat[i])/(double)(stall[i])*100+"%");
+        }
+
         System.out.println(TimeStamp() + " <"+code+"> Task completed (Client " + clientName + ") command "+cmd);
         return 0;
     }
